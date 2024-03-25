f7b7978e5627e279d34eecb440eab326a1e832ee
Rita <rita.radiun@gmail.com>
2024-03-25 21:11:35 +0200

    cfg klaida

--- src/cfg/cfg.js
+++ src/cfg/cfg.js
@@ -1,7 +1,7 @@
 export const cfg = {
   API: {
     HOST:
-      process.env.NODR_ENV === 'production'
+      process.env.NODE_ENV === 'production'
         ? 'https://api-shop-rr.vercel.app'
         : 'http://localhost:3000',
   },
